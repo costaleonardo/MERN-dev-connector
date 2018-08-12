6376e23487e894eed03823655e5d92372161301d
costaleonardo <costaleonardo97@gmail.com>
2018-08-12 09:36:25 -0400

    Create JWT.

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://costaleonardo:leo123@ds119702.mlab.com:19702/dev-connector'
+    mongoURI: 'mongodb://costaleonardo:leo123@ds119702.mlab.com:19702/dev-connector',
+    secretOrKey: 'secret'
 }
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
 
 // Load User model
 const User = require('../../models/User');
@@ -48,31 +50,43 @@ router.post('/register', (req, res) => {
         });
 });
 
-// @route  GET api/users/login
-// @desc   Login user / Returning JWT token
-// @access Public
+// @route   GET api/users/login
+// @desc    Login User / Returning JWT Token
+// @access  Public
 router.post('/login', (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
-
+  
     // Find user by email
-    User.findOne({email})
-        .then(user => {
-            // Check for user
-            if (!user) {
-                return res.status(404).json({ email: 'User not found.' });
+    User.findOne({ email }).then(user => {
+      // Check for user
+      if (!user) {
+        return res.status(404).json({ msg: 'User not found.'});
+      }
+  
+      // Check Password
+      bcrypt.compare(password, user.password).then(isMatch => {
+        if (isMatch) {
+          // User Matched
+          const payload = { id: user.id, name: user.name, avatar: user.avatar }; // Create JWT Payload
+  
+          // Sign Token
+          jwt.sign(
+            payload,
+            keys.secretOrKey,
+            { expiresIn: 3600 },
+            (err, token) => {
+              res.json({
+                success: true,
+                token: 'Bearer ' + token
+              });
             }
-
-            // Check Password
-            bcrypt.compare(password, user.password)
-                .then(isMatch => {
-                    if (isMatch) {
-                        res.json({ msg: 'Success' });
-                    } else {
-                        return res.status(400).json({ password: 'Password incorrect.' });
-                    }
-                });
-        })
-});
+          );
+        } else {
+          return res.status(400).json({ msg: 'Password incorrect.'});
+        }
+      });
+    });
+  });
 
 module.exports = router;
