c8ae8f39f8c3dda935ba0b9934e0bb8dce548939
costaleonardo <costaleonardo97@gmail.com>
2018-08-18 13:11:14 -0400

    Refactor /client/src/reducers.

--- client/src/reducers/errorReducer.js
+++ client/src/reducers/errorReducer.js
@@ -2,10 +2,10 @@ import { GET_ERRORS, CLEAR_ERRORS } from '../actions/types';
 
 const initialState = {};
 
-export default function (state = initialState, action) {
+export default function(state = initialState, action) {
   switch (action.type) {
     case GET_ERRORS:
-      return action.payload
+      return action.payload;
     case CLEAR_ERRORS:
       return {};
     default:
