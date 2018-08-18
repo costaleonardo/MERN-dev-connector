9a4ec816a84518d316de6202432fdea2e906b21a
costaleonardo <costaleonardo97@gmail.com>
2018-08-18 12:31:11 -0400

    Refactor /config.

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-    mongoURI: 'mongodb://costaleonardo:leo123@ds119702.mlab.com:19702/dev-connector',
-    secretOrKey: 'secret'
-}
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys_prod');
+  } else {
+    module.exports = require('./keys_dev');
+  }
